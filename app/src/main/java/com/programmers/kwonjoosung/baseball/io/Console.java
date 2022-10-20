6381b1b04c771801802e39e6b8b1bc32831504ba
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-10-21 01:21:26 +0900

    [update] Console
    * I/O Inteface 메소드 구현

--- app/src/main/java/com/programmers/kwonjoosung/baseball/io/Console.java
+++ app/src/main/java/com/programmers/kwonjoosung/baseball/io/Console.java
@@ -1,26 +1,24 @@
 package com.programmers.kwonjoosung.baseball.io;
 
-import com.programmers.kwonjoosung.baseball.io.Input;
-import com.programmers.kwonjoosung.baseball.io.Output;
+import java.util.Scanner;
 
 public class Console implements Input, Output {
+    Scanner scanner = new Scanner(System.in);
     @Override
     public String input(String Message) {
-        return null;
+        print(Message);
+        return scanner.nextLine();
     }
-
     @Override
     public void print(String data) {
-
+        System.out.println(data);
     }
-
     @Override
     public void win() {
-
+        System.out.println("정답입니다.");
     }
-
     @Override
     public void inputError() {
-
+        System.out.println("잘못입력하셨습니다");
     }
 }
