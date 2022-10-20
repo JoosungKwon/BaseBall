bbbfb1e238f7e3fe08cfbd2501dd76d21115af64
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-10-20 17:00:01 +0900

    [add] Console
    * create Console
    * I/O Interface의 구현체

---
+++ app/src/main/java/com/programmers/kwonjoosung/baseball/Console.java
@@ -0,0 +1,14 @@
+package com.programmers.kwonjoosung.baseball;
+
+public class Console <I extends String,O extends String> implements Input<I>,Output<O>{
+
+    @Override
+    public I input() {
+        return null;
+    }
+
+    @Override
+    public void print(O data) {
+
+    }
+}
