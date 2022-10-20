e8b3911656dd449df4e38901fc5dcd5dc653d70b
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-10-20 18:43:13 +0900

    [update] Console
    * 메서드 변경 -> 단순 입출력 플랫폼 이상의 역할을 위해서
    * 폴더 이동

---
+++ app/src/main/java/com/programmers/kwonjoosung/baseball/io/Console.java
@@ -0,0 +1,26 @@
+package com.programmers.kwonjoosung.baseball.io;
+
+import com.programmers.kwonjoosung.baseball.io.Input;
+import com.programmers.kwonjoosung.baseball.io.Output;
+
+public class Console implements Input, Output {
+    @Override
+    public String input(String Message) {
+        return null;
+    }
+
+    @Override
+    public void print(String data) {
+
+    }
+
+    @Override
+    public void win() {
+
+    }
+
+    @Override
+    public void inputError() {
+
+    }
+}
