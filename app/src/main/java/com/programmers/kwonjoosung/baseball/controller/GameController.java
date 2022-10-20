f5845ab9a4cdf1cda9e35f9d50a7311deae7b657
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-10-21 01:14:55 +0900

    [add] Game
    * BaseBall -> GameController 클래스명 변경
    * run() 메소드 구현

--- app/src/main/java/com/programmers/kwonjoosung/baseball/controller/GameController.java
+++ app/src/main/java/com/programmers/kwonjoosung/baseball/controller/GameController.java
@@ -2,10 +2,13 @@ package com.programmers.kwonjoosung.baseball.controller;
 
 import com.programmers.kwonjoosung.baseball.io.Input;
 import com.programmers.kwonjoosung.baseball.io.Output;
+import com.programmers.kwonjoosung.baseball.model.BallCount;
 import com.programmers.kwonjoosung.baseball.utils.Calculator;
 import com.programmers.kwonjoosung.baseball.utils.NumberGenerator;
 import com.programmers.kwonjoosung.baseball.utils.Parser;
 import lombok.AllArgsConstructor;
+
+import java.util.Optional;
 
 /*
  * 숫자 야구 게임 설계
@@ -26,20 +29,40 @@ import lombok.AllArgsConstructor;
  * <핵심로직 설계하기> - sudo code
  * 1. 정답으로 사용할 랜덤 숫자 생성하기
  * 2. (Loop)입력장치를 이용해 사용자에게 입력받아오기
- * 3. 입력값을 Parser에게 전달하여 올바른 값인지 검사하기 -> 예외 발생
+ * 3. 입력값을 Parser에게 전달하여 올바른 값인지 검사하기 -> 예외 발생(에러 출력)
  * 4. 계산기를 통해 정답과 얼마나 일치하는지 계산하기
  * 5. 계산된 결과를 BallCount 객체에 담아 출력하기
  * 6. (Break)정답인 경우 승리를 출력하고 종료
  */
 @AllArgsConstructor
-public class BaseBall {
-    Input in;
-    Output out;
-    NumberGenerator numberGenerate;
-    Parser parser;
-    Calculator calculate;
+public class GameController {
+    private Input in;
+    private Output out;
+    private NumberGenerator numberGenerator;
+    private Parser parser;
+    private Calculator calculator;
+    private final int COUNT_NUMBER = 3;
     public void run(){
+        String[] answer = numberGenerator.getNumber(COUNT_NUMBER); // 랜덤 숫자 생성
 
+        while(true){
 
+            String data = in.input("숫자를 입력하세요."); // 입력값 받기
+            Optional<String[]> reply = parser.parsing(data,COUNT_NUMBER); //  데이터 파싱하기 (올바른 입력인지 확인)
+
+            if(reply.isEmpty()){ // 만약 응답 데이터가 올바르지 않다면 에러 출력
+                out.inputError();
+                continue;
+            }
+
+            BallCount ballCount = calculator.calculate(reply.get(),answer); // 결과를 계산한다.
+
+            out.print(ballCount.toString()); // 결과 출력
+
+            if(ballCount.getStrike() == COUNT_NUMBER){ // 승리 조건 만족시 승리 출력 후 종료
+                out.win();
+                break;
+            }
+        }
     }
 }
--- app/src/main/java/com/programmers/kwonjoosung/baseball/utils/Parser.java
+++ app/src/main/java/com/programmers/kwonjoosung/baseball/utils/Parser.java
@@ -1,4 +1,20 @@
 package com.programmers.kwonjoosung.baseball.utils;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class Parser {
+    public Optional<String[]> parsing(String data, int COUNT_NUM) {
+
+        long cnt = data
+                .chars()
+                .filter(Character::isDigit)
+                .map(Character::getNumericValue)
+                .distinct()
+                .filter(i -> i > 0)
+                .count();
+        if(cnt != COUNT_NUM) return Optional.empty();
+        String[] reply = data.split("");
+        return Optional.of(reply);
+    }
 }
