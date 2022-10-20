09c23475453086f16212a9c1cbbaea4ca2e42868
권주성 <99165624+JoosungKwon@users.noreply.github.com>
2022-10-20 18:48:28 +0900

    구조 설계 완료

--- app/src/main/java/com/programmers/kwonjoosung/baseball/App.java
+++ app/src/main/java/com/programmers/kwonjoosung/baseball/App.java
@@ -1,7 +1,19 @@
 package com.programmers.kwonjoosung.baseball;
 
+import com.programmers.kwonjoosung.baseball.controller.BaseBall;
+import com.programmers.kwonjoosung.baseball.io.Console;
+import com.programmers.kwonjoosung.baseball.utils.Calculator;
+import com.programmers.kwonjoosung.baseball.utils.FakeNumberGenerator;
+import com.programmers.kwonjoosung.baseball.utils.NumberGenerator;
+import com.programmers.kwonjoosung.baseball.utils.Parser;
+
 public class App {
     public static void main(String[] args) {
-        new BaseBall().run();
+        Console console = new Console();
+        NumberGenerator numberGenerate = new FakeNumberGenerator();
+        Calculator calculator = new Calculator();
+        Parser parser = new Parser();
+
+        new BaseBall(console,console,numberGenerate,parser,calculator).run();
     }
 }
